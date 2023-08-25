043e9effa1ec9131165978a24429f503a64cab49
Keith Horwood <keithwhor@gmail.com>
2023-08-25 14:31:47 -0700

    Update init tools

--- commands/init.js
+++ commands/init.js
@@ -14,12 +14,26 @@ class NewCommand extends Command {
     return {
       description: 'Generates new objects.\nSupported object_names are: migration',
       args: ['object_name'],
-      flags: {flag: 'An example flag'},
-      vflags: {vflag: 'An example verbose flag'}
+      flags: {},
+      vflags: {force: 'Force initialization of an Instant.dev project'}
     };
   }
 
   async run (params) {
+
+    const force = ('force' in params.vflags);
+
+    if (!force && Instant.isFilesystemInitialized()) {
+      throw new Error(
+        `Instant.dev has already been initialized in "${Instant.filesystemRoot()}".\n\n` +
+        `You can force a new initialization with:\n\n` +
+        colors.grey.bold(`\t$ instant init --force\n\n`) +
+        `which will reset your migrations but preserve your database.\n\n` +
+        `If you simply want to reset your data, run:\n\n` +
+        colors.grey.bold(`\t$ instant db:bootstrap\n\n`) +
+        `which will empty your database, re-run all migrations, and seed your data.`
+      );
+    }
 
     console.log();
     console.log(`Welcome to ${colors.green.bold('Instant.dev')}!`);
@@ -74,12 +88,27 @@ class NewCommand extends Command {
       throw new Error(`Could not connect to database: ${e.message}`);
     }
 
-    Instant.Migrator.enableDangerous();
     Instant.enableLogs(2);
-
+    Instant.Migrator.enableDangerous();
     Instant.Migrator.Dangerous.reset();
     await Instant.Migrator.Dangerous.prepare();
     await Instant.Migrator.Dangerous.initialize();
+    Instant.Migrator.disableDangerous();
+
+    console.log();
+    console.log(colors.bold.green(`Instant.dev initialized successfully!`));
+    console.log();
+    console.log(`You can create a new migration with:`);
+    console.log();
+    console.log(
+      colors.grey.bold(
+        `\t$ instant new migration`
+      )
+    );
+    console.log();
+    console.log(`Please visit [URL] for more information about using Instant.dev.`);
+    console.log(colors.green.bold(`Happy building! :)`));
+    console.log();
 
     return void 0;
 
