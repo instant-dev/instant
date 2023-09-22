05b08bae0235db9b11a7701c4b0de2400b2db844
Keith Horwood <keithwhor@gmail.com>
2023-09-21 17:14:51 -0700

    Update version checking

--- helpers/load_instant.js
+++ helpers/load_instant.js
@@ -12,44 +12,76 @@ const drawBox = (...text) => {
   let minPad = 2;
   let length = maxLength + minPad * 2;
   return [].concat(
-    ``,
     `╔` + `═`.repeat(length) + `╗`,
     lines.map(line => {
       let count = (length - stripColors(line).length) / 2;
       return `║` + ` `.repeat(count) + line + ` `.repeat(count) + `║`;
     }),
     `╚` + `═`.repeat(length) + `╝`,
-    ``
   ).join('\n');
 };
 
 module.exports = async (validate = false, checkVersion = false) => {
 
-  const pkg = require('../package.json');
-  const {name, version} = pkg;
+  const pkgs = {self: require('../package.json')};
   try {
-    const response = await new Promise(resolve => {
-      https.request(`https://registry.npmjs.org/${name}/latest`, res => {
-        const buffers = [];
-        res.on('data', data => buffers.push(data));
-        res.on('end', () => resolve(Buffer.concat(buffers)));
-      }).end();
-    });
-    const json = JSON.parse(response.toString());
-    if (json.version !== version) {
-      console.log(
-        drawBox(
-          `A new version of the instant.dev CLI is available:`,
-          ``,
-          `${version} -> ${colors.bold.green(json.version)}`,
-          ``,
-          `upgrade: ${colors.bold.green(`npm i instant.dev@latest -g`)}`
-        )
-      )
-    }
+    pkgs.orm = require(path.join(process.cwd(), '/node_modules/@instant.dev/orm/package.json'));
   } catch (e) {
-    console.error(e);
-    throw new Error(`Error validating CLI version`);
+    // do nothing
+  }
+  const packages = [
+    {
+      title: 'Instant CLI',
+      name: pkgs.self.name,
+      version: pkgs.self.version,
+      global: true
+    },
+    {
+      title: 'Instant ORM',
+      name: pkgs.orm ? pkgs.orm.name : null,
+      version: pkgs.orm ? pkgs.orm.version : null
+    }
+  ];
+  const checkPackages = packages.filter(name => !!name);
+  const verifiedPackages = await Promise.all(
+    checkPackages.map(pkg => {
+      return (async () => {
+        try {
+          const response = await new Promise(resolve => {
+            https.request(`https://registry.npmjs.org/${pkg.name}/latest`, res => {
+              const buffers = [];
+              res.on('data', data => buffers.push(data));
+              res.on('end', () => resolve(Buffer.concat(buffers)));
+            }).end();
+          });
+          const json = JSON.parse(response.toString());
+          pkg.latest = json.version;
+          return pkg;
+        } catch (e) {
+          console.error(e);
+          throw new Error(`Error validating ${pkg.title} version`);
+        }
+      })();
+    })
+  );
+  const updatePackages = verifiedPackages.filter(pkg => pkg.latest !== pkg.version);
+  if (updatePackages.length) {
+    console.log();
+    console.log(
+      drawBox(
+        ``,
+        `Updates are available for ${colors.bold('instant.dev')}:`,
+        ``,
+        ...updatePackages.map(pkg => {
+          return [
+            pkg.title,
+            `${pkg.version} -> ${colors.bold.green(pkg.latest)}`,
+            `${colors.bold.grey(`npm i ${pkg.name}@latest${pkg.global ? ' -g' : ''}`)}`,
+            ``
+          ].join('\n')
+        })
+      )
+    );
   }
 
   const pathname = path.join(process.cwd(), 'node_modules', '@instant.dev/orm');
